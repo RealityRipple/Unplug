bc914c3a256e2ad3fcdede1ad9daec603790cffa
Andy <webmaster@RealityRipple.com>
2021-07-21 23:32:14 -0700

    Add about:newplugin URL

--- chrome/content/overlay.js
+++ chrome/content/overlay.js
@@ -2,6 +2,7 @@ var unplug =
 {
  init: function()
  {
+  AboutNewPluginService.register();
   window.setInterval(unplug.check, 10000);
   window.setTimeout(unplug.check, 1000);
  },
@@ -24,7 +25,7 @@ var unplug =
    return;
   for (let i = 0; i < newPlugins.length; i++)
   {
-   unplug.showPage('chrome://unplug/content/newplugin.xul?id=' + newPlugins[i].niceName);
+   unplug.showPage('about:newplugin?id=' + newPlugins[i].niceName);
   }
  },
  showPage: function(url)
@@ -58,6 +59,52 @@ var unplug =
    return prefs.getBoolPref(prefName);
   else
    return defVal;
+ }
+};
+
+var AboutNewPluginService = {
+ impl:
+ {
+  ioService: null,
+  newChannel: function(aURI, aLoadInfo)
+  {
+   if (!AboutNewPluginService.ioService)
+    AboutNewPluginService.ioService = Components.classes['@mozilla.org/network/io-service;1'].getService(Components.interfaces.nsIIOService);
+   let uri = AboutNewPluginService.ioService.newURI('chrome://unplug/content/newplugin.xul', null, null);
+   let chan = AboutNewPluginService.ioService.newChannelFromURIWithLoadInfo(uri, aLoadInfo);
+   chan.originalURI = aURI;
+   return chan;
+  },
+  QueryInterface: function(iid)
+  {
+   if (!iid.equals(Components.interfaces.nsISupports) && !iid.equals(Components.interfaces.nsIAboutModule))
+    throw Components.results.NS_ERROR_NO_INTERFACE;
+   return this;
+  },
+  getURIFlags: function(aURI)
+  {
+   return Components.interfaces.nsIAboutModule.ALLOW_SCRIPT | Components.interfaces.nsIAboutModule.HIDE_FROM_ABOUTABOUT;
+  }
+ },
+ factory:
+ {
+  createInstance: function(outer, iid)
+  {
+   if (outer !== null)
+    throw Components.results.NS_ERROR_NO_AGGREGATION;
+   if (!iid.equals(Components.interfaces.nsIAboutModule) && !iid.equals(Components.interfaces.nsISupports))
+    throw Components.results.NS_ERROR_INVALID_ARG;
+   return AboutNewPluginService.impl.QueryInterface(iid);
+  }
+ },
+ register: function()
+ {
+  let compman = Components.manager;
+  compman.QueryInterface(Components.interfaces.nsIComponentRegistrar);
+  let cid = Components.ID('{88A17F3D-2B10-5F12-8ED8-0C9DC7EC3784}');
+  let contractid = '@mozilla.org/network/protocol/about;1?what=newplugin';
+  if (!compman.isCIDRegistered(cid))
+   compman.registerFactory(cid, 'AboutNewPluginService', contractid, AboutNewPluginService.factory);
  }
 };
 
